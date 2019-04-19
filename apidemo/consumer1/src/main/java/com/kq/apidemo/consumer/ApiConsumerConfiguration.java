4104bc03de7e65e590ca13326f99c6d4f84636df
king <kongq1983@qq.com>
2019-04-20 01:26:38 +0800

    add api demo

---
+++ apidemo/consumer1/src/main/java/com/kq/apidemo/consumer/ApiConsumerConfiguration.java
@@ -0,0 +1,40 @@
+package com.kq.apidemo.consumer;
+
+
+import com.kq.api.IDemoService;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+
+/**
+ * api方式配置dubbo客户端示例
+ * ApiConsumerConfiguration
+ */
+public class ApiConsumerConfiguration {
+
+	public static void main(String[] args) {
+
+		String address = "zookeeper://127.0.0.1:2181";
+		// 当前应用配置
+		ApplicationConfig application = new ApplicationConfig();
+		application.setName("consumer-of-helloworld-app");
+
+		// 连接注册中心配置
+		RegistryConfig registry = new RegistryConfig(address, "dubbo");
+
+		// 注意：ReferenceConfig为重对象，内部封装了与注册中心的连接，以及与服务提供方的连接
+		// 引用远程服务
+		// 此实例很重，封装了与注册中心的连接以及与提供者的连接，请自行缓存，否则可能造成内存和连接泄漏
+		ReferenceConfig<IDemoService> reference = new ReferenceConfig<IDemoService>();
+		reference.setApplication(application);
+		reference.setRegistry(registry); // 多个注册中心可以用setRegistries()
+		reference.setInterface(IDemoService.class);
+		reference.setVersion("1.0.0");
+
+		// 和本地bean一样使用demoService
+		IDemoService demoService = reference.get(); // 注意：此代理对象内部封装了所有通讯细节，对象较重，请缓存复用
+		String hello = demoService.getMessage("API demo");
+		System.out.println(hello);
+	}
+
+}
---
+++ apidemo/provider1/src/main/java/com/kq/apidemo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.kq.apidemo.service.impl;
+
+import com.kq.api.IDemoService;
+import org.apache.dubbo.config.annotation.Service;
+
+/**
+ * DemoServiceImpl
+ *
+ * @author kq
+ * @date 2019/4/19
+ */
+
+@Service(version = "${demo.service.version}")
+public class DemoServiceImpl implements IDemoService {
+
+    public String getMessage(String name) {
+        return "Hello, "+name;
+    }
+
+
+}
