151e0e334b8437467a656b4d0d933d850b6eddf2
king <kongq1983@qq.com>
2019-06-17 00:17:09 +0800

    client demo

--- dubboclient/src/main/java/com/kq/server/MyClient.java
+++ dubboclient/src/main/java/com/kq/server/MyClient.java
@@ -5,6 +5,7 @@ import com.kq.util.ByteUtil;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
 
 /**
  * MyClient
@@ -15,7 +16,8 @@ import java.nio.channels.SocketChannel;
 public class MyClient {
 
     public static void main(String[] args) throws Exception{
-        send("localhost",8300);
+//        send("localhost",8300);
+        send("192.168.3.102",8300);
     }
 
     private static void send(String host,int port) throws Exception{
@@ -31,24 +33,8 @@ public class MyClient {
 //
 //        // 发送数据 - header段
 //        byte[] header = new byte[16];
-        // 魔数，short类型 (类似 class文件里面的cafebabb) 数据的开头
-        byte[] magicArray = ByteUtil.short2bytes((short) 0xdabb);
-
-        byte[] mesBytes = "i'm client!".getBytes();
-
-        System.out.println("send data length="+mesBytes.length);
-
-        byte[] length = ByteUtil.int2bytes(mesBytes.length);
 
 
-        byte[] array = new byte[magicArray.length+length.length+mesBytes.length];
-
-        // magicArray copy to array
-        System.arraycopy(magicArray,0,array,0,magicArray.length);
-        // length copy to array
-        System.arraycopy(length,0,array,2,length.length);
-        // mesBytes copy to array
-        System.arraycopy(mesBytes,0,array,6,mesBytes.length);
 
 
 
@@ -82,7 +68,7 @@ public class MyClient {
 //        System.out.println("finishConnect="+finishConnect);
 
 
-        dubboClient.write(ByteBuffer.wrap(array));
+
 
         ByteBuffer response = ByteBuffer.allocate(1025);
         System.out.println("start response length="+response.position());
@@ -91,7 +77,38 @@ public class MyClient {
         System.out.println("响应内容：");
         System.out.println(new String(response.array()));
 
+        for(int i=1;i<=100;i++) {
+            byte[] array = getSendBytes(i);
+            Thread.sleep(100);
+            dubboClient.write(ByteBuffer.wrap(array));
+            dubboClient.socket().getOutputStream().flush();
+        }
 
+        TimeUnit.SECONDS.sleep(600);
+
+    }
+
+    private static byte[] getSendBytes(int i) {
+        // 魔数，short类型 (类似 class文件里面的cafebabb) 数据的开头
+        byte[] magicArray = ByteUtil.short2bytes((short) 0xdabb);
+
+        String messgae = i+",i'm client!";
+        byte[] mesBytes = messgae.getBytes();
+
+        System.out.println("send data length="+mesBytes.length+",index="+i);
+
+        byte[] length = ByteUtil.int2bytes(mesBytes.length);
+
+
+        byte[] array = new byte[magicArray.length+length.length+mesBytes.length];
+
+        // magicArray copy to array
+        System.arraycopy(magicArray,0,array,0,magicArray.length);
+        // length copy to array
+        System.arraycopy(length,0,array,2,length.length);
+        // mesBytes copy to array
+        System.arraycopy(mesBytes,0,array,6,mesBytes.length);
+        return array;
     }
 
 }
